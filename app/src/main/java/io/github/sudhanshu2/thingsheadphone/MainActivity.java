16d7d542bb7bf0e3b2d1ec312e768f38555ceadf
Sudhanshu Agarwal <sudhanshu@res380d-128-61-83-211.res.gatech.edu>
2019-02-16 21:12:16 -0500

    Initial Android Things App

---
+++ app/src/main/java/io/github/sudhanshu2/thingsheadphone/MainActivity.java
@@ -0,0 +1,20 @@
+package io.github.sudhanshu2.thingsheadphone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
