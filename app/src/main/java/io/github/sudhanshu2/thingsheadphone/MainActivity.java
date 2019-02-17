984faf5eba24661b8e667d0f90f2cb2e50e8e2c1
Sudhanshu Agarwal <sudhanshu@res380d-128-61-83-211.res.gatech.edu>
2019-02-16 22:04:23 -0500

    Minor test

--- app/src/main/java/io/github/sudhanshu2/thingsheadphone/MainActivity.java
+++ app/src/main/java/io/github/sudhanshu2/thingsheadphone/MainActivity.java
@@ -1,20 +1,88 @@
 package io.github.sudhanshu2.thingsheadphone;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.contrib.driver.apa102.Apa102;
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int LEDSTRIP_BRIGHTNESS = 1;
 
+    private AlphanumericDisplay mDisplay;
+    private Apa102 mLedstrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "Weather Station Started");
+
+        // Initialize 14-segment display
+        try {
+            mDisplay = RainbowHat.openDisplay();
+            mDisplay.setEnabled(true);
+            mDisplay.display("0000");
+            Log.d(TAG, "Initialized I2C Display");
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing display", e);
+        }
+
+        // Initialize LED strip
+        try {
+            mLedstrip = RainbowHat.openLedStrip();
+            mLedstrip.setBrightness(LEDSTRIP_BRIGHTNESS);
+            int[] colors = new int[7];
+            colors[0] = Color.parseColor("#FF0000");
+            colors[1] = Color.parseColor("#FF7F00");
+            colors[2] = Color.parseColor("#FFFF00");
+            colors[3] = Color.parseColor("#00FF00");
+            colors[4] = Color.parseColor("#0000FF");
+            colors[5] = Color.parseColor("#4B0082");
+            colors[6] = Color.parseColor("#9400D3");
+            mLedstrip.write(colors);
+            // Because of a known APA102 issue, write the initial value twice.
+            mLedstrip.write(colors);
+
+            Log.d(TAG, "Initialized SPI LED strip");
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing LED strip", e);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        if (mDisplay != null) {
+            try {
+                mDisplay.clear();
+                mDisplay.setEnabled(false);
+                mDisplay.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing display", e);
+            } finally {
+                mDisplay = null;
+            }
+        }
+
+        if (mLedstrip != null) {
+            try {
+                mLedstrip.setBrightness(0);
+                mLedstrip.write(new int[7]);
+                mLedstrip.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing LED strip", e);
+            } finally {
+                mLedstrip = null;
+            }
+        }
     }
 
 }
